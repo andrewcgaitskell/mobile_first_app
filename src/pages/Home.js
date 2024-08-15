e2256b0ab81f0138eec8847753f31696bd64ce57
andrewcgaitskell <andrew@gaitskell.com>
2024-08-15 09:59:30 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to MyApp</h1>
+      <p>This is a mobile-first React app with multi-page routing.</p>
+    </div>
+  );
+};
+
+export default Home;
